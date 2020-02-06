e18ac1e3cb57d68f01829263db7e9f0c00afe39f
JLG <mjgaumnitz@kc.rr.com>
2020-02-06 14:06:25 -0600

    added build folder and mlab credentials for Heroku deployment

---
+++ client/build/precache-manifest.ab0708ade48d465ad251ca97d6f2fa83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5e7c0a3bb8fb939be73b63ae416e61b",
+    "url": "/static/media/bookswallpaper.e5e7c0a3.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9f17b333fde9d9e039a7",
+    "url": "/static/js/main.d9ce13c8.chunk.js"
+  },
+  {
+    "revision": "2c95bda1c3708af149a8",
+    "url": "/static/js/2.1402404a.chunk.js"
+  },
+  {
+    "revision": "9f17b333fde9d9e039a7",
+    "url": "/static/css/main.2afe77a0.chunk.css"
+  },
+  {
+    "revision": "d18d295dcd551633c8181ae311ef5a04",
+    "url": "/index.html"
+  }
+];
